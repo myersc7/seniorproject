aa703ef8838617902e1e0839b5f8b8322c738b51
xian14 <jeku_15@live.com>
2018-04-21 15:55:58 -0400

    Change secret key to be randomly generated

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
     # change it as needed
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
     SQLALCHEMY_DATABASE_URI = 'mysql://root:Ytiliga2018**@ec2-54-157-47-97.compute-1.amazonaws.com/senior_project2'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
