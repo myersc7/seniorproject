9f2c6be2051577ad06921891e4731060eac5a644
christophermyers <myersc7@students.rowan.edu>
2018-04-20 21:59:51 -0400

    Buttons now use solid color instead of image. Deleted unnecessary static files. Altered/streamlines css/js links.

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
     # change it as needed
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Ytiliga2018**@localhost/senior_project'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Ytiliga2018**@ec2-54-157-47-97.compute-1.amazonaws.com/senior_project2'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
