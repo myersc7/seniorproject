dc74c624f900e8f8b27f18f05123647d89ed34cf
xian14 <jeku_15@live.com>
2018-04-30 16:00:21 -0400

    Added ip to accept for any incoming in run.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
+
 class Config(object):
     # change it as needed
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Ytiliga2018**@ec2-54-157-47-97.compute-1.amazonaws.com/senior_project'
+    #SQLALCHEMY_DATABASE_URI = 'mysql://root:Ytiliga2018**@ec2-54-157-47-97.compute-1.amazonaws.com/senior_project'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/senior_project'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
